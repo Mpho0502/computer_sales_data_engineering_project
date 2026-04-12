57e0336c6cb7bc8a199ab5164c5e8e678d6eccce
Mpho <matebesimoe89@gmail.com>
2026-04-12 11:47:13 +0200

    drop dim_location, create new table, insert ID column

--- scripts_sql/dimensions/dim_location/dim_location.sql
+++ scripts_sql/dimensions/dim_location/dim_location.sql
@@ -6,3 +6,12 @@ INTO [computer_stg].[dbo].[dim_location]
 FROM [computer_stg].[dbo].[raw_pc_data]
 
 --inserting location ID column
+DROP TABLE [computer_stg].[dbo].[dim_location]
+CREATE TABLE [computer_stg].[dbo].[dim_location](
+	[LocationID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Continent] [nvarchar](50) NOT NULL,
+	[Country_or_State] [nvarchar](50) NOT NULL,
+	[Province_or_City] [nvarchar](100) NOT NULL
+)
+
+INSERT INTO [computer_stg].[dbo].[dim_location] (

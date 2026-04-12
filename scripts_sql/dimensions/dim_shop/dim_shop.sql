35177e6d9fe2317bfbedb05917d10acd80f6f6f1
Mpho <matebesimoe89@gmail.com>
2026-04-12 12:12:04 +0200

    drop dim_shop, create new table, insert ID column

--- scripts_sql/dimensions/dim_shop/dim_shop.sql
+++ scripts_sql/dimensions/dim_shop/dim_shop.sql
@@ -3,3 +3,11 @@ SELECT DISTINCT [Shop_Name]
       ,[Shop_Age]
 INTO [computer_stg].[dbo].[dim_shop]
 FROM [computer_stg].[dbo].[raw_pc_data]
+
+--Inserting shop ID column
+DROP TABLE [computer_stg].[dbo].[dim_shop]
+CREATE TABLE [computer_stg].[dbo].[dim_shop](
+	[ShopID] INT IDENTITY (1,1) PRIMARY KEY,
+	[Shop_Name] [nvarchar](50) NOT NULL,
+	[Shop_Age] [nvarchar](50) NOT NULL
+)

2401496810d4576b48a6734cf461e2084da23d1e
Mpho <matebesimoe89@gmail.com>
2026-04-13 12:17:47 +0200

    adding fact sales table script

---
+++ scripts_sql/fact/fact_table/sales_fact_table_stg.sql
@@ -0,0 +1,11 @@
+--PC sales fact table
+SELECT DISTINCT [Cost_Price]
+      ,[Sale_Price]
+      ,[Discount_Amount]
+      ,[Finance_Amount]
+      ,[Credit_Score]
+      ,[Cost_of_Repairs]
+      ,[Total_Sales_per_Employee]
+      ,[PC_Market_Price]
+  INTO [computer_stg].[dbo].[pc_sales_fact]
+  FROM [computer_stg].[dbo].[raw_pc_data]

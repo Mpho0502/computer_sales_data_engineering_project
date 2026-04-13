4a50a238f1dfc07bb47392f91f3b77217232de89
Mpho <matebesimoe89@gmail.com>
2026-04-13 11:17:20 +0200

    drop dim_date, create new table, insert ID column

--- scripts_sql/dimensions/dim_date/dim_date.sql
+++ scripts_sql/dimensions/dim_date/dim_date.sql
@@ -3,3 +3,10 @@ SELECT DISTINCT [Purchase_Date]
                ,[Ship_Date]
 INTO [computer_stg].[dbo].[dim_date]
 FROM [computer_stg].[dbo].[raw_pc_data]
+
+--Inserting date ID column
+DROP TABLE [computer_stg].[dbo].[dim_date]
+CREATE TABLE [computer_stg].[dbo].[dim_date](
+	[Purchase_Date] [datetime2](7) NOT NULL,
+	[Ship_Date] [nvarchar](50) NOT NULL
+)

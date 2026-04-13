2f4415c4bfa7b9e9cf058ef25e7c828d4462258b
Mpho <matebesimoe89@gmail.com>
2026-04-13 09:51:54 +0200

    drop dim_pc, create new table, insert ID column

--- scripts_sql/dimensions/dim_pc/dim_pc.sql
+++ scripts_sql/dimensions/dim_pc/dim_pc.sql
@@ -3,3 +3,11 @@ SELECT DISTINCT [PC_Make]
                 ,[PC_Model]
 INTO [computer_stg].[dbo].[dim_pc]
 FROM [computer_stg].[dbo].[raw_pc_data]
+
+--Insert pc ID column
+DROP TABLE [computer_stg].[dbo].[dim_pc]
+CREATE TABLE [computer_stg].[dbo].[dim_pc](
+	[PC_ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[PC_Make] [nvarchar](50) NOT NULL,
+	[PC_Model] [nvarchar](50) NOT NULL
+)

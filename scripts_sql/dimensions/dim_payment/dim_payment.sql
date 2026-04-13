361a16a7f0474b8e4683eab8b0db3bca3ffcaee2
Mpho <matebesimoe89@gmail.com>
2026-04-13 11:26:14 +0200

    drop dim_payment, create new table, insert ID column

--- scripts_sql/dimensions/dim_payment/dim_payment.sql
+++ scripts_sql/dimensions/dim_payment/dim_payment.sql
@@ -2,3 +2,11 @@
 SELECT DISTINCT [Payment_Method]
 INTO [computer_stg].[dbo].[dim_payment]
 FROM [computer_stg].[dbo].[raw_pc_data]
+
+--Inserting payment ID column
+DROP TABLE [computer_stg].[dbo].[dim_payment]
+CREATE TABLE [computer_stg].[dbo].[dim_payment](
+	[PaymentID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Payment_Method] [nvarchar](50) NOT NULL
+)
+

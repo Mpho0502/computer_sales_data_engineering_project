2296a19a93fe9689fd4221f769c141478fd79dea
Mpho <matebesimoe89@gmail.com>
2026-04-13 10:20:45 +0200

    insert distinct values from raw data into dim storage

--- scripts_sql/dimensions/dim_storage/dim_storage.sql
+++ scripts_sql/dimensions/dim_storage/dim_storage.sql
@@ -13,3 +13,14 @@ CREATE TABLE [computer_stg].[dbo].[dim_storage](
 	[RAM] [nvarchar](50) NOT NULL,
 	[Storage_Capacity] [nvarchar](50) NOT NULL
 )
+
+--Insert values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_storage](
+	[Storage_Type],
+	[RAM],
+	[Storage_Capacity]
+)
+SELECT DISTINCT [Storage_Type],
+				[RAM],
+				[Storage_Capacity]
+FROM [computer_stg].[dbo].[raw_pc_data]

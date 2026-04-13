f3e3315e920c18410d5c714fb997c64e99cfbafa
Mpho <matebesimoe89@gmail.com>
2026-04-13 10:57:49 +0200

    drop dim_sales_person, create new table, insert ID column

--- scripts_sql/dimensions/dim_sales_person/dim_sales_person.sql
+++ scripts_sql/dimensions/dim_sales_person/dim_sales_person.sql
@@ -3,3 +3,12 @@ SELECT DISTINCT  [Sales_Person_Name]
                 ,[Sales_Person_Department]
 INTO [computer_stg].[dbo].[dim_sales_person]
 FROM [computer_stg].[dbo].[raw_pc_data]
+
+--Insert sales person ID column
+DROP TABLE [computer_stg].[dbo].[dim_sales_person]
+CREATE TABLE [computer_stg].[dbo].[dim_sales_person](
+	[SalesPersonID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Sales_Person_Name] [nvarchar](50) NOT NULL,
+	[Sales_Person_Department] [nvarchar](50) NOT NULL
+)
+

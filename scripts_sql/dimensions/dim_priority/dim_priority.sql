70fd2a9f1ceb3aa4ac0eb313f61cc2fdfcb9b9c8
Mpho <matebesimoe89@gmail.com>
2026-04-13 11:51:42 +0200

    insert distinct values from raw data into dim priority

--- scripts_sql/dimensions/dim_priority/dim_priority.sql
+++ scripts_sql/dimensions/dim_priority/dim_priority.sql
@@ -9,3 +9,10 @@ CREATE TABLE [computer_stg].[dbo].[dim_priority](
 	[PriorityID] INT IDENTITY(1,1) PRIMARY KEY,
 	[Priority] [nvarchar](50) NOT NULL
 ) 
+
+--Insert values into the table from the raw data
+INSERT INTO [computer_stg].[dbo].[dim_priority](
+	[Priority]
+)
+SELECT DISTINCT [Priority]
+FROM [computer_stg].[dbo].[raw_pc_data]

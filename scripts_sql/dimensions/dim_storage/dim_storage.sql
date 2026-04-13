88bb4d2e71fa65b9abfca7965009db04fe613c9c
Mpho <matebesimoe89@gmail.com>
2026-04-13 10:16:48 +0200

    drop dim_storage, create new table, insert ID column

--- scripts_sql/dimensions/dim_storage/dim_storage.sql
+++ scripts_sql/dimensions/dim_storage/dim_storage.sql
@@ -4,3 +4,12 @@ SELECT DISTINCT [Storage_Type]
                 ,[Storage_Capacity]
 INTO [computer_stg].[dbo].[dim_storage]
 FROM [computer_stg].[dbo].[raw_pc_data]
+
+--Inserting storage ID column
+DROP TABLE [computer_stg].[dbo].[dim_storage]
+CREATE TABLE [computer_stg].[dbo].[dim_storage](
+	[StorageID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Storage_Type] [nvarchar](50) NOT NULL,
+	[RAM] [nvarchar](50) NOT NULL,
+	[Storage_Capacity] [nvarchar](50) NOT NULL
+)

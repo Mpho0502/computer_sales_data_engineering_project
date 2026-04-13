547256766a94abc0408b9c9fda1f233e642bcc3b
Mpho <matebesimoe89@gmail.com>
2026-04-13 12:20:43 +0200

    drop sales fact table, create new table, insert ID column

--- scripts_sql/fact/fact_table/sales_fact_table_stg.sql
+++ scripts_sql/fact/fact_table/sales_fact_table_stg.sql
@@ -9,3 +9,17 @@ SELECT DISTINCT [Cost_Price]
       ,[PC_Market_Price]
   INTO [computer_stg].[dbo].[pc_sales_fact]
   FROM [computer_stg].[dbo].[raw_pc_data]
+
+  --Insert Sales ID column
+  DROP TABLE [computer_stg].[dbo].[pc_sales_fact]
+  CREATE TABLE [computer_stg].[dbo].[pc_sales_fact](
+	[SalesID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Cost_Price] [int] NOT NULL,
+	[Sale_Price] [int] NOT NULL,
+	[Discount_Amount] [int] NOT NULL,
+	[Finance_Amount] [nvarchar](50) NOT NULL,
+	[Credit_Score] [int] NOT NULL,
+	[Cost_of_Repairs] [nvarchar](50) NOT NULL,
+	[Total_Sales_per_Employee] [int] NOT NULL,
+	[PC_Market_Price] [int] NOT NULL
+)

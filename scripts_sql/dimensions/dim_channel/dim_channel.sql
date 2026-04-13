20072ba8efc8c294f50dbde5b2964c345f7867cf
Mpho <matebesimoe89@gmail.com>
2026-04-13 11:37:40 +0200

    drop dim_channel, create new table, insert ID column

--- scripts_sql/dimensions/dim_channel/dim_channel.sql
+++ scripts_sql/dimensions/dim_channel/dim_channel.sql
@@ -2,3 +2,11 @@
 SELECT DISTINCT [Channel]
 INTO [computer_stg].[dbo].[dim_channel]
 FROM [computer_stg].[dbo].[raw_pc_data]
+
+--Insert channel ID column
+DROP TABLE [computer_stg].[dbo].[dim_channel]
+CREATE TABLE [computer_stg].[dbo].[dim_channel](
+	[Channel_ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Channel] [nvarchar](50) NOT NULL
+)
+

e23b1c88a408fcc74fcfb430afdfa78081814506
Mpho <matebesimoe89@gmail.com>
2026-04-13 11:29:12 +0200

    insert distinct values from raw data into dim payment

--- scripts_sql/dimensions/dim_payment/dim_payment.sql
+++ scripts_sql/dimensions/dim_payment/dim_payment.sql
@@ -10,3 +10,9 @@ CREATE TABLE [computer_stg].[dbo].[dim_payment](
 	[Payment_Method] [nvarchar](50) NOT NULL
 )
 
+--Insert values into the table from the raw data
+INSERT INTO [computer_stg].[dbo].[dim_payment](
+	[Payment_Method]
+)
+SELECT DISTINCT [Payment_Method]
+FROM [computer_stg].[dbo].[raw_pc_data]

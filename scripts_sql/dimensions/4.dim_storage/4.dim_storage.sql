9eddf90239ad3a4cc3a04ac10ee307dedd5f0308
Mpho <matebesimoe89@gmail.com>
2026-04-17 14:17:21 +0200

    cleaning dim storage code

--- scripts_sql/dimensions/4.dim_storage/4.dim_storage.sql
+++ scripts_sql/dimensions/4.dim_storage/4.dim_storage.sql
@@ -1,24 +1,26 @@
 --create table
-DROP TABLE [computer_stg].[dbo].[dim_storage]
-CREATE TABLE [computer_stg].[dbo].[dim_storage](
-	[StorageID] INT IDENTITY(1,1) PRIMARY KEY,
+DROP TABLE [computer_stg].[dbo].[dim_storage] CREATE TABLE [computer_stg].[dbo].[dim_storage](
+	[StorageID] INT IDENTITY(1, 1) PRIMARY KEY,
 	[Storage_Type] [nvarchar](50) NOT NULL,
 	[RAM] [nvarchar](50) NOT NULL,
 	[Storage_Capacity] [nvarchar](50) NOT NULL,
 	[Load_date] DATETIME DEFAULT GETDATE()
-)
-
+) 
 --Insert values into the table from raw data
-INSERT INTO [computer_stg].[dbo].[dim_storage](
-	[Storage_Type],
+INSERT INTO
+	[computer_stg].[dbo].[dim_storage](
+		[Storage_Type],
+		[RAM],
+		[Storage_Capacity]
+	)
+SELECT
+	DISTINCT [Storage_Type],
 	[RAM],
 	[Storage_Capacity]
-)
-SELECT DISTINCT [Storage_Type],
-				[RAM],
-				[Storage_Capacity]
-FROM [computer_stg].[dbo].[raw_pc_data]
-
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
 --Show all the storage insight
-SELECT *
-FROM [computer_stg].[dbo].[dim_storage]
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_storage]

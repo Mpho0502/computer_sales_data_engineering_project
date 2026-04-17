f157c776198587cde3d5e5c1c163c69d48935b71
Mpho <matebesimoe89@gmail.com>
2026-04-17 14:08:19 +0200

    cleaning dim pc code

--- scripts_sql/dimensions/3.dim_pc/3.dim_pc.sql
+++ scripts_sql/dimensions/3.dim_pc/3.dim_pc.sql
@@ -1,21 +1,20 @@
 --Create table
-DROP TABLE [computer_stg].[dbo].[dim_pc]
-CREATE TABLE [computer_stg].[dbo].[dim_pc](
-	[PC_ID] INT IDENTITY(1,1) PRIMARY KEY,
+DROP TABLE [computer_stg].[dbo].[dim_pc] CREATE TABLE [computer_stg].[dbo].[dim_pc](
+	[PC_ID] INT IDENTITY(1, 1) PRIMARY KEY,
 	[PC_Make] [nvarchar](50) NOT NULL,
 	[PC_Model] [nvarchar](50) NOT NULL,
 	[Load_date] DATETIME DEFAULT GETDATE()
-)
-
+) 
 --Insert values into the table from raw data
-INSERT INTO [computer_stg].[dbo].[dim_pc](
-	[PC_Make],
+INSERT INTO
+	[computer_stg].[dbo].[dim_pc]([PC_Make], [PC_Model])
+SELECT
+	DISTINCT [PC_Make],
 	[PC_Model]
-)
-SELECT DISTINCT [PC_Make],
-				[PC_Model]
-FROM [computer_stg].[dbo].[raw_pc_data]
-
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
 --Show all the PCs
-SELECT *
-FROM [computer_stg].[dbo].[dim_pc]
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_pc]

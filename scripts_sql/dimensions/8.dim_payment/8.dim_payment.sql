f40861ddcaf88a520efe9e2c75c1f28b76cd6dba
Mpho <matebesimoe89@gmail.com>
2026-04-17 14:22:57 +0200

    cleaning dim payment code

--- scripts_sql/dimensions/8.dim_payment/8.dim_payment.sql
+++ scripts_sql/dimensions/8.dim_payment/8.dim_payment.sql
@@ -1,18 +1,18 @@
 --create table
-DROP TABLE [computer_stg].[dbo].[dim_payment]
-CREATE TABLE [computer_stg].[dbo].[dim_payment](
-	[PaymentID] INT IDENTITY(1,1) PRIMARY KEY,
+DROP TABLE [computer_stg].[dbo].[dim_payment] CREATE TABLE [computer_stg].[dbo].[dim_payment](
+	[PaymentID] INT IDENTITY(1, 1) PRIMARY KEY,
 	[Payment_Method] [nvarchar](50) NOT NULL,
 	[Load_date] DATETIME DEFAULT GETDATE()
-)
-
+) 
 --Insert values into the table from the raw data
-INSERT INTO [computer_stg].[dbo].[dim_payment](
-	[Payment_Method]
-)
-SELECT DISTINCT [Payment_Method]
-FROM [computer_stg].[dbo].[raw_pc_data]
-
+INSERT INTO
+	[computer_stg].[dbo].[dim_payment]([Payment_Method])
+SELECT
+	DISTINCT [Payment_Method]
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
 --Show all payment methods
-SELECT *
-FROM [computer_stg].[dbo].[dim_payment]
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_payment]

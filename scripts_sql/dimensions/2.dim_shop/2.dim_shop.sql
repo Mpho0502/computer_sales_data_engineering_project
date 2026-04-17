70dd44b48bf3f0ea9ea52e7a2cee70fc25c31ccb
Mpho <matebesimoe89@gmail.com>
2026-04-17 12:47:04 +0200

    cleaning the code in the dim table, adding load date column

--- scripts_sql/dimensions/2.dim_shop/2.dim_shop.sql
+++ scripts_sql/dimensions/2.dim_shop/2.dim_shop.sql
@@ -1,21 +1,16 @@
---Dim Shop
-SELECT DISTINCT [Shop_Name]
-      ,[Shop_Age]
-INTO [computer_stg].[dbo].[dim_shop]
-FROM [computer_stg].[dbo].[raw_pc_data]
-
---Inserting shop ID column
+--Create table
 DROP TABLE [computer_stg].[dbo].[dim_shop]
 CREATE TABLE [computer_stg].[dbo].[dim_shop](
 	[ShopID] INT IDENTITY (1,1) PRIMARY KEY,
 	[Shop_Name] [nvarchar](50) NOT NULL,
-	[Shop_Age] [nvarchar](50) NOT NULL
+	[Shop_Age] [nvarchar](50) NOT NULL,
+	[Load_date] DATETIME DEFAULT GETDATE()
 )
 
 --Insert values into the table from the raw data
 INSERT INTO [computer_stg].[dbo].[dim_shop] (
-		[Shop_Name], 
-		[Shop_Age]
+		    [Shop_Name], 
+		    [Shop_Age]
 )
 SELECT DISTINCT [Shop_Name], 
 				[Shop_Age]

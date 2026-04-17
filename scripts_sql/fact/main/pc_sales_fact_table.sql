8aee512a5fc50d71b3f1b695f5d31c88cdc044a4
Mpho <matebesimoe89@gmail.com>
2026-04-17 13:57:20 +0200

    cleaning initial fact table data

--- scripts_sql/fact/main/pc_sales_fact_table.sql
+++ scripts_sql/fact/main/pc_sales_fact_table.sql
@@ -1,12 +1,13 @@
 --Create a fact table with all quantitative data from the raw data.
 --PC sales fact table
-SELECT DISTINCT [Cost_Price]
-      ,[Sale_Price]
-      ,[Discount_Amount]
-      ,[Finance_Amount]
-      ,[Credit_Score]
-      ,[Cost_of_Repairs]
-      ,[Total_Sales_per_Employee]
-      ,[PC_Market_Price]
-  INTO [computer_stg].[dbo].[pc_sales_fact]
-  FROM [computer_stg].[dbo].[raw_pc_data]
+SELECT
+    DISTINCT [Cost_Price],
+    [Sale_Price],
+    [Discount_Amount],
+    [Finance_Amount],
+    [Credit_Score],
+    [Cost_of_Repairs],
+    [Total_Sales_per_Employee],
+    [PC_Market_Price] INTO [computer_stg].[dbo].[pc_sales_fact]
+FROM
+    [computer_stg].[dbo].[raw_pc_data]

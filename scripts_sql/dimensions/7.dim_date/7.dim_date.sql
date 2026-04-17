f07053f198f322297e4b2652c35170feea6a09c1
Mpho <matebesimoe89@gmail.com>
2026-04-17 14:19:44 +0200

    cleaning dim date code

--- scripts_sql/dimensions/7.dim_date/7.dim_date.sql
+++ scripts_sql/dimensions/7.dim_date/7.dim_date.sql
@@ -1,21 +1,20 @@
 --create table
-DROP TABLE [computer_stg].[dbo].[dim_date]
-CREATE TABLE [computer_stg].[dbo].[dim_date](
-	[DateID] INT IDENTITY(1,1) PRIMARY KEY,
+DROP TABLE [computer_stg].[dbo].[dim_date] CREATE TABLE [computer_stg].[dbo].[dim_date](
+	[DateID] INT IDENTITY(1, 1) PRIMARY KEY,
 	[Purchase_Date] [datetime2](7) NOT NULL,
 	[Ship_Date] [nvarchar](50) NOT NULL,
 	[Load_date] DATETIME DEFAULT GETDATE()
-	)
-
+) 
 --Insert values into the table from raw data
-INSERT INTO [computer_stg].[dbo].[dim_date](
-	[Purchase_Date],
+INSERT INTO
+	[computer_stg].[dbo].[dim_date]([Purchase_Date], [Ship_Date])
+SELECT
+	DISTINCT [Purchase_Date],
 	[Ship_Date]
-)
-SELECT DISTINCT [Purchase_Date],
-				[Ship_Date] 
-FROM [computer_stg].[dbo].[raw_pc_data]
-
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
 --Show all the dates
-SELECT *
-FROM [computer_stg].[dbo].[dim_date]
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_date]

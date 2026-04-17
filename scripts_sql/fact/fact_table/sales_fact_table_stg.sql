e96ed40aea81a0f30ad1aefa9ae6266f2101f031
Mpho <matebesimoe89@gmail.com>
2026-04-17 14:32:38 +0200

    rearranging dims on fact table in order

--- scripts_sql/fact/fact_table/sales_fact_table_stg.sql
+++ scripts_sql/fact/fact_table/sales_fact_table_stg.sql
@@ -1,16 +1,16 @@
 --create table
 DROP TABLE [computer_stg].[dbo].[pc_sales_fact] CREATE TABLE [computer_stg].[dbo].[pc_sales_fact](
 	[SalesID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[CustomerID] INT,
-	[Channel_ID] INT,
-	[DateID] INT,
 	[LocationID] INT,
-	[PaymentID] INT,
-	[PC_ID] INT,
-	[PriorityID] INT,
-	[SalesPersonID] INT,
 	[ShopID] INT,
+	[PC_ID] INT,
 	[StorageID] INT,
+	[CustomerID] INT,
+	[SalesPersonID] INT,
+	[DateID] INT,
+	[PaymentID] INT,
+	[Channel_ID] INT,
+	[PriorityID] INT,
 	[Cost_Price] [int] NOT NULL,
 	[Sale_Price] [int] NOT NULL,
 	[Discount_Amount] [int] NOT NULL,
@@ -19,19 +19,18 @@ DROP TABLE [computer_stg].[dbo].[pc_sales_fact] CREATE TABLE [computer_stg].[dbo
 	[Cost_of_Repairs] [nvarchar](50) NOT NULL,
 	[Total_Sales_per_Employee] [int] NOT NULL,
 	[PC_Market_Price] [int] NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE() 
-	CONSTRAINT fk_CustomerID foreign key (CustomerID) references [computer_stg].[dbo].[dim_customer] (CustomerID),
-	CONSTRAINT fk_Channel_ID foreign key (Channel_ID) references [computer_stg].[dbo].[dim_channel] (Channel_ID),
-	CONSTRAINT fk_DateID foreign key (DateID) references [computer_stg].[dbo].[dim_date] (DateID),
+	[Load_date] DATETIME DEFAULT GETDATE(),
 	CONSTRAINT fk_LocationID foreign key (LocationID) references [computer_stg].[dbo].[dim_location] (LocationID),
-	CONSTRAINT fk_PaymentID foreign key (PaymentID) references [computer_stg].[dbo].[dim_payment] (PaymentID),
-	CONSTRAINT fk_PC_ID foreign key (PC_ID) references [computer_stg].[dbo].[dim_pc] (PC_ID),
-	CONSTRAINT fk_PriorityID foreign key (PriorityID) references [computer_stg].[dbo].[dim_priority] (PriorityID),
-	CONSTRAINT fk_SalesPersonID foreign key (SalesPersonID) references [computer_stg].[dbo].[dim_sales_person] (SalesPersonID),
 	CONSTRAINT fk_ShopID foreign key (ShopID) references [computer_stg].[dbo].[dim_shop] (ShopID),
-	CONSTRAINT fk_StorageID foreign key (StorageID) references [computer_stg].[dbo].[dim_storage] (StorageID)
-) 
---Insert values into table from raw data
+	CONSTRAINT fk_PC_ID foreign key (PC_ID) references [computer_stg].[dbo].[dim_pc] (PC_ID),
+	CONSTRAINT fk_StorageID foreign key (StorageID) references [computer_stg].[dbo].[dim_storage] (StorageID),
+	CONSTRAINT fk_CustomerID foreign key (CustomerID) references [computer_stg].[dbo].[dim_customer] (CustomerID),
+	CONSTRAINT fk_SalesPersonID foreign key (SalesPersonID) references [computer_stg].[dbo].[dim_sales_person] (SalesPersonID),
+	CONSTRAINT fk_DateID foreign key (DateID) references [computer_stg].[dbo].[dim_date] (DateID),
+	CONSTRAINT fk_PaymentID foreign key (PaymentID) references [computer_stg].[dbo].[dim_payment] (PaymentID),
+	CONSTRAINT fk_Channel_ID foreign key (Channel_ID) references [computer_stg].[dbo].[dim_channel] (Channel_ID),
+	CONSTRAINT fk_PriorityID foreign key (PriorityID) references [computer_stg].[dbo].[dim_priority] (PriorityID),
+) --Insert values into table from raw data
 INSERT INTO
 	[computer_stg].[dbo].[pc_sales_fact](
 		[Cost_Price],
@@ -53,8 +52,7 @@ SELECT
 	[Total_Sales_per_Employee],
 	[PC_Market_Price]
 FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the sales
+	[computer_stg].[dbo].[raw_pc_data] --Show all the sales
 SELECT
 	*
 FROM

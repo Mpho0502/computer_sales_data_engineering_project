71221d25f05f2d621bc470f4c051ffbbf83b50e4
Mpho <matebesimoe89@gmail.com>
2026-04-17 14:19:08 +0200

    cleaning dim sales person code

--- scripts_sql/dimensions/6.dim_sales_person/6.dim_sales_person.sql
+++ scripts_sql/dimensions/6.dim_sales_person/6.dim_sales_person.sql
@@ -1,21 +1,23 @@
 --create table
-DROP TABLE [computer_stg].[dbo].[dim_sales_person]
-CREATE TABLE [computer_stg].[dbo].[dim_sales_person](
-	[SalesPersonID] INT IDENTITY(1,1) PRIMARY KEY,
+DROP TABLE [computer_stg].[dbo].[dim_sales_person] CREATE TABLE [computer_stg].[dbo].[dim_sales_person](
+	[SalesPersonID] INT IDENTITY(1, 1) PRIMARY KEY,
 	[Sales_Person_Name] [nvarchar](50) NOT NULL,
 	[Sales_Person_Department] [nvarchar](50) NOT NULL,
 	[Load_date] DATETIME DEFAULT GETDATE()
-)
-
+) 
 --Insert values into tables from raw data
-INSERT INTO [computer_stg].[dbo].[dim_sales_person](
-	[Sales_Person_Name],
+INSERT INTO
+	[computer_stg].[dbo].[dim_sales_person](
+		[Sales_Person_Name],
+		[Sales_Person_Department]
+	)
+SELECT
+	DISTINCT [Sales_Person_Name],
 	[Sales_Person_Department]
-)
-SELECT DISTINCT [Sales_Person_Name],
-				[Sales_Person_Department]
-FROM [computer_stg].[dbo].[raw_pc_data]
-
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
 --Show all the sales persons
-SELECT *
-FROM [computer_stg].[dbo].[dim_sales_person]
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_sales_person]

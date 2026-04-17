20f41cdf2568f95c67e071f95dca472ddc52ff8d
Mpho <matebesimoe89@gmail.com>
2026-04-17 14:18:08 +0200

    cleaning dim customer code

--- scripts_sql/dimensions/5.dim_customer/5.dim_customer.sql
+++ scripts_sql/dimensions/5.dim_customer/5.dim_customer.sql
@@ -1,27 +1,29 @@
 --create table
-DROP TABLE [computer_stg].[dbo].[dim_customer]
-CREATE TABLE [computer_stg].[dbo].[dim_customer](
-	[CustomerID] INT IDENTITY(1,1) PRIMARY KEY,
+DROP TABLE [computer_stg].[dbo].[dim_customer] CREATE TABLE [computer_stg].[dbo].[dim_customer](
+	[CustomerID] INT IDENTITY(1, 1) PRIMARY KEY,
 	[Customer_Name] [nvarchar](50) NOT NULL,
 	[Customer_Surname] [nvarchar](50) NOT NULL,
 	[Customer_Contact_Number] [nvarchar](50) NOT NULL,
 	[Customer_Email_Address] [nvarchar](50) NOT NULL,
 	[Load_date] DATETIME DEFAULT GETDATE()
-)
-
+) 
 --Insert values into the table from raw data
-INSERT INTO [computer_stg].[dbo].[dim_customer](
-	[Customer_Name],
+INSERT INTO
+	[computer_stg].[dbo].[dim_customer](
+		[Customer_Name],
+		[Customer_Surname],
+		[Customer_Contact_Number],
+		[Customer_Email_Address]
+	)
+SELECT
+	DISTINCT [Customer_Name],
 	[Customer_Surname],
 	[Customer_Contact_Number],
 	[Customer_Email_Address]
-)
-SELECT DISTINCT [Customer_Name],
-				[Customer_Surname],
-				[Customer_Contact_Number],
-				[Customer_Email_Address]
-FROM [computer_stg].[dbo].[raw_pc_data]
-
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
 --Show all the customers
-SELECT *
-FROM [computer_stg].[dbo].[dim_customer]
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_customer]

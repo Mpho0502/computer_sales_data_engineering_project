d595b8d942357b21db9fe41f9873581501573292
Mpho <matebesimoe89@gmail.com>
2026-04-17 12:00:18 +0200

    updating database code

--- scripts_sql/database/database_stg_&_dwh.sql
+++ scripts_sql/database/database_stg_&_dwh.sql
@@ -1,2 +1,19 @@
-CREATE DATABASE computer_stg
-CREATE DATABASE computer_dwh
+--CREATE DATABASE computer_stg
+--CREATE DATABASE computer_dwh
+
+--
+
+IF DB_ID('computer_stg') IS NULL
+BEGIN
+    CREATE DATABASE computer_stg;
+END
+GO
+
+--
+
+IF DB_ID('computer_dwh') IS NULL
+BEGIN
+    CREATE DATABASE computer_dwh;
+END
+GO
+

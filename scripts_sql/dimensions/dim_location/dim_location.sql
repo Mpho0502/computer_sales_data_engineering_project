f50dfa0c7358703f4f956a1ed459a216574b1103
Mpho <matebesimoe89@gmail.com>
2026-04-17 11:40:54 +0200

    cleaning dim loaction code, adding time date

--- scripts_sql/dimensions/dim_location/dim_location.sql
+++ scripts_sql/dimensions/dim_location/dim_location.sql
@@ -1,17 +1,11 @@
---Dim Location
-SELECT DISTINCT [Continent]
-      ,[Country_or_State]
-      ,[Province_or_City]
-INTO [computer_stg].[dbo].[dim_location]
-FROM [computer_stg].[dbo].[raw_pc_data]
-
---inserting location ID column
+--create table
 DROP TABLE [computer_stg].[dbo].[dim_location]
 CREATE TABLE [computer_stg].[dbo].[dim_location](
 	[LocationID] INT IDENTITY(1,1) PRIMARY KEY,
 	[Continent] [nvarchar](50) NOT NULL,
 	[Country_or_State] [nvarchar](50) NOT NULL,
-	[Province_or_City] [nvarchar](100) NOT NULL
+	[Province_or_City] [nvarchar](100) NOT NULL,
+	[Load_date] DATETIME DEFAULT GETDATE()
 )
 
 --Insert values into the table from Raw data

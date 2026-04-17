3c69af38673b4495b5de5dc83510cd68f920ddf2
Mpho <matebesimoe89@gmail.com>
2026-04-17 13:51:59 +0200

    cleaning database code

--- scripts_sql/database/database_stg_&_dwh.sql
+++ scripts_sql/database/database_stg_&_dwh.sql
@@ -1,19 +1,12 @@
 --CREATE DATABASE computer_stg
 --CREATE DATABASE computer_dwh
-
 --
+IF DB_ID('computer_stg') IS NULL BEGIN CREATE DATABASE computer_stg;
 
-IF DB_ID('computer_stg') IS NULL
-BEGIN
-    CREATE DATABASE computer_stg;
 END
 GO
-
 --
+    IF DB_ID('computer_dwh') IS NULL BEGIN CREATE DATABASE computer_dwh;
 
-IF DB_ID('computer_dwh') IS NULL
-BEGIN
-    CREATE DATABASE computer_dwh;
 END
 GO
-
--- scripts_sql/dimensions/#main/#dimension_tables.sql
+++ scripts_sql/dimensions/#main/#dimension_tables.sql
@@ -5,48 +5,57 @@ SELECT
       [Country_or_State],
       [Province_or_City] INTO [computer_stg].[dbo].[dim_location]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Shop
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Shop
 SELECT
       DISTINCT [Shop_Name],
       [Shop_Age] INTO [computer_stg].[dbo].[dim_shop]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim PC
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim PC
 SELECT
       DISTINCT [PC_Make],
       [PC_Model] INTO [computer_stg].[dbo].[dim_pc]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Storage
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Storage
 SELECT
       DISTINCT [Storage_Type],
       [RAM],
       [Storage_Capacity] INTO [computer_stg].[dbo].[dim_storage]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Customer
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Customer
 SELECT
       DISTINCT [Customer_Name],
       [Customer_Surname],
       [Customer_Contact_Number],
       [Customer_Email_Address] INTO [computer_stg].[dbo].[dim_customer]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Sales Person
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Sales Person
 SELECT
       DISTINCT [Sales_Person_Name],
       [Sales_Person_Department] INTO [computer_stg].[dbo].[dim_sales_person]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Date
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Date
 SELECT
       DISTINCT [Purchase_Date],
       [Ship_Date] INTO [computer_stg].[dbo].[dim_date]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Payment
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Payment
 SELECT
       DISTINCT [Payment_Method] INTO [computer_stg].[dbo].[dim_payment]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Channel
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Channel
 SELECT
       DISTINCT [Channel] INTO [computer_stg].[dbo].[dim_channel]
 FROM
-      [computer_stg].[dbo].[raw_pc_data] --Dim Priority
+      [computer_stg].[dbo].[raw_pc_data] 
+--Dim Priority
 SELECT
       DISTINCT [Priority] INTO [computer_stg].[dbo].[dim_priority]
 FROM

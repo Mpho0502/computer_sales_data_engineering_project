8cc1993390fc2d4b799f88b6bc26960d732190b1
Mpho <matebesimoe89@gmail.com>
2026-04-22 16:13:59 +0200

    created a sp for create dim pc

---
+++ scripts_sql/stored_procedures/3.sp_create_dim_pc.sql
@@ -0,0 +1,25 @@
+CREATE PROCEDURE sp_create_dim_pc
+AS
+BEGIN
+  -- SQL_statements to be executed
+	 --Create table
+DROP TABLE [computer_stg].[dbo].[dim_pc] CREATE TABLE [computer_stg].[dbo].[dim_pc](
+	[PC_ID] INT IDENTITY(1, 1) PRIMARY KEY,
+	[PC_Make] [nvarchar](50) NOT NULL,
+	[PC_Model] [nvarchar](50) NOT NULL,
+	[Load_date] DATETIME DEFAULT GETDATE()
+) 
+		--Insert values into the table from raw data
+INSERT INTO
+	[computer_stg].[dbo].[dim_pc]([PC_Make], [PC_Model])
+SELECT
+	DISTINCT [PC_Make],
+	[PC_Model]
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
+		--Show all the PCs
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_pc]
+END;

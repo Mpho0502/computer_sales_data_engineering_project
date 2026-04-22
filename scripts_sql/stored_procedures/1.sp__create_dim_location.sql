4192a64eeb411ef211ae52cb5367fcb9645f5b82
Mpho <matebesimoe89@gmail.com>
2026-04-22 16:06:09 +0200

    created a sp for create dim location

---
+++ scripts_sql/stored_procedures/1.sp__create_dim_location.sql
@@ -0,0 +1,31 @@
+CREATE PROCEDURE sp_create_dim_location
+AS
+BEGIN
+  -- SQL_statements to be executed
+	--create table
+DROP TABLE [computer_stg].[dbo].[dim_location] CREATE TABLE [computer_stg].[dbo].[dim_location](
+	[LocationID] INT IDENTITY(1, 1) PRIMARY KEY,
+	[Continent] [nvarchar](50) NOT NULL,
+	[Country_or_State] [nvarchar](50) NOT NULL,
+	[Province_or_City] [nvarchar](100) NOT NULL,
+	[Load_date] DATETIME DEFAULT GETDATE()
+) 
+	--Insert values into the table from Raw data
+INSERT INTO
+	[computer_stg].[dbo].[dim_location] (
+		[Continent],
+		[Country_or_State],
+		[Province_or_City]
+	)
+SELECT
+	DISTINCT [Continent],
+	[Country_or_State],
+	[Province_or_City]
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
+	--Show all the locations
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_location]
+END;

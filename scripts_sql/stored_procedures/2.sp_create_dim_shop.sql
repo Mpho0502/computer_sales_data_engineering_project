8d413a8a8a774d81a8e456b5c88588c16685e264
Mpho <matebesimoe89@gmail.com>
2026-04-22 16:10:31 +0200

    created a sp for create dim shop

---
+++ scripts_sql/stored_procedures/2.sp_create_dim_shop.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE sp_create_dim_shop
+ 
+AS
+BEGIN
+  -- SQL_statements to be executed
+		--Create table
+DROP TABLE [computer_stg].[dbo].[dim_shop] CREATE TABLE [computer_stg].[dbo].[dim_shop](
+	[ShopID] INT IDENTITY (1, 1) PRIMARY KEY,
+	[Shop_Name] [nvarchar](50) NOT NULL,
+	[Shop_Age] [nvarchar](50) NOT NULL,
+	[Load_date] DATETIME DEFAULT GETDATE()
+) 
+		--Insert values into the table from the raw data
+INSERT INTO
+	[computer_stg].[dbo].[dim_shop] ([Shop_Name], [Shop_Age])
+SELECT
+	DISTINCT [Shop_Name],
+	[Shop_Age]
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
+		--Show all the shops
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_shop]
+END;

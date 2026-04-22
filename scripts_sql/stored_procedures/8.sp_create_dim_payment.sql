baf726961bcdcca0cf33018b741bf6251ed612ba
Mpho <matebesimoe89@gmail.com>
2026-04-22 16:37:48 +0200

    created a sp for create dim payment

---
+++ scripts_sql/stored_procedures/8.sp_create_dim_payment.sql
@@ -0,0 +1,23 @@
+CREATE PROCEDURE sp_create_dim_payment
+AS
+BEGIN
+  -- SQL_statements to be executed
+	--create table
+DROP TABLE [computer_stg].[dbo].[dim_payment] CREATE TABLE [computer_stg].[dbo].[dim_payment](
+	[PaymentID] INT IDENTITY(1, 1) PRIMARY KEY,
+	[Payment_Method] [nvarchar](50) NOT NULL,
+	[Load_date] DATETIME DEFAULT GETDATE()
+) 
+	--Insert values into the table from the raw data
+INSERT INTO
+	[computer_stg].[dbo].[dim_payment]([Payment_Method])
+SELECT
+	DISTINCT [Payment_Method]
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
+	--Show all payment methods
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_payment]
+END;

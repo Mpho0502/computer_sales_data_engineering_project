3c9c5fcf4efcd0ef2e88c980b67779396794ac77
Mpho <matebesimoe89@gmail.com>
2026-04-22 16:26:36 +0200

    created a sp for create dim customer

---
+++ scripts_sql/stored_procedures/5.sp_create_dim_customer.sql
@@ -0,0 +1,34 @@
+CREATE PROCEDURE sp_create_dim_customer
+AS
+BEGIN
+  -- SQL_statements to be executed
+	--create table
+DROP TABLE [computer_stg].[dbo].[dim_customer] CREATE TABLE [computer_stg].[dbo].[dim_customer](
+	[CustomerID] INT IDENTITY(1, 1) PRIMARY KEY,
+	[Customer_Name] [nvarchar](50) NOT NULL,
+	[Customer_Surname] [nvarchar](50) NOT NULL,
+	[Customer_Contact_Number] [nvarchar](50) NOT NULL,
+	[Customer_Email_Address] [nvarchar](50) NOT NULL,
+	[Load_date] DATETIME DEFAULT GETDATE()
+) 
+	--Insert values into the table from raw data
+INSERT INTO
+	[computer_stg].[dbo].[dim_customer](
+		[Customer_Name],
+		[Customer_Surname],
+		[Customer_Contact_Number],
+		[Customer_Email_Address]
+	)
+SELECT
+	DISTINCT [Customer_Name],
+	[Customer_Surname],
+	[Customer_Contact_Number],
+	[Customer_Email_Address]
+FROM
+	[computer_stg].[dbo].[raw_pc_data] 
+	--Show all the customers
+SELECT
+	*
+FROM
+	[computer_stg].[dbo].[dim_customer]
+END;

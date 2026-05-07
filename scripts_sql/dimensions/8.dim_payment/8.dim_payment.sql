e84b4de8fc0aaf267dee356a2320282ada48b12e
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:57:01 +0200

    cleaning dim_payment script

--- scripts_sql/dimensions/8.dim_payment/8.dim_payment.sql
+++ scripts_sql/dimensions/8.dim_payment/8.dim_payment.sql
@@ -1,18 +1,24 @@
---create table
-DROP TABLE [computer_stg].[dbo].[dim_payment] CREATE TABLE [computer_stg].[dbo].[dim_payment](
-	[PaymentID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Payment_Method] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from the raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_payment]([Payment_Method])
-SELECT
-	DISTINCT [Payment_Method]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all payment methods
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_payment]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_payment]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_payment](
+        [PaymentID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Payment_Method] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_payment] (
+    [Payment_Method]
+)
+SELECT DISTINCT
+    [Payment_Method]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all payment methods
+SELECT *
+FROM [computer_stg].[dbo].[dim_payment];
+GO

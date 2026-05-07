a7e0ec9257836e7679f8f07f3ed363650409c3f2
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:48:26 +0200

    cleaning dim_date script

--- scripts_sql/dimensions/7.dim_date/7.dim_date.sql
+++ scripts_sql/dimensions/7.dim_date/7.dim_date.sql
@@ -1,20 +1,27 @@
---create table
-DROP TABLE [computer_stg].[dbo].[dim_date] CREATE TABLE [computer_stg].[dbo].[dim_date](
-	[DateID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Purchase_Date] [datetime2](7) NOT NULL,
-	[Ship_Date] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_date]([Purchase_Date], [Ship_Date])
-SELECT
-	DISTINCT [Purchase_Date],
-	[Ship_Date]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the dates
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_date]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_date]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_date](
+        [DateID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Purchase_Date] DATETIME2(7) NOT NULL,
+        [Ship_Date] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_date] (
+    [Purchase_Date],
+    [Ship_Date]
+)
+SELECT DISTINCT
+    [Purchase_Date],
+    [Ship_Date]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the dates
+SELECT *
+FROM [computer_stg].[dbo].[dim_date];
+GO

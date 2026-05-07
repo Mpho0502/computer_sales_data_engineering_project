208ce533dcb20b45f1ddacfadab2ea0f91f190e9
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:33:16 +0200

    cleaning dim_pc script

--- scripts_sql/dimensions/3.dim_pc/3.dim_pc.sql
+++ scripts_sql/dimensions/3.dim_pc/3.dim_pc.sql
@@ -1,20 +1,27 @@
---Create table
-DROP TABLE [computer_stg].[dbo].[dim_pc] CREATE TABLE [computer_stg].[dbo].[dim_pc](
-	[PC_ID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[PC_Make] [nvarchar](50) NOT NULL,
-	[PC_Model] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_pc]([PC_Make], [PC_Model])
-SELECT
-	DISTINCT [PC_Make],
-	[PC_Model]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the PCs
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_pc]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_pc]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_pc](
+        [PC_ID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [PC_Make] NVARCHAR(50) NOT NULL,
+        [PC_Model] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_pc] (
+    [PC_Make],
+    [PC_Model]
+)
+SELECT DISTINCT
+    [PC_Make],
+    [PC_Model]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the PCs
+SELECT *
+FROM [computer_stg].[dbo].[dim_pc];
+GO

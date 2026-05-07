781f210c128b09a17e87c113f0fdb8019b100907
Mpho <matebesimoe89@gmail.com>
2026-05-07 19:00:54 +0200

    cleaning dim_channel script

--- scripts_sql/dimensions/9.dim_channel/9.dim_channel.sql
+++ scripts_sql/dimensions/9.dim_channel/9.dim_channel.sql
@@ -1,18 +1,24 @@
---create table
-DROP TABLE [computer_stg].[dbo].[dim_channel] CREATE TABLE [computer_stg].[dbo].[dim_channel](
-	[Channel_ID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Channel] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from the raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_channel]([Channel])
-SELECT
-	DISTINCT [Channel]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the channels
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_channel]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_channel]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_channel](
+        [Channel_ID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Channel] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_channel] (
+    [Channel]
+)
+SELECT DISTINCT
+    [Channel]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the channels
+SELECT *
+FROM [computer_stg].[dbo].[dim_channel];
+GO

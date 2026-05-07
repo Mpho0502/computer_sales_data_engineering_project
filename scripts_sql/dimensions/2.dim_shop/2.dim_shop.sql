51f31a86d8572a2c24a6a42178fe8d12a5d903a8
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:28:46 +0200

    cleaning dim_shop script

--- scripts_sql/dimensions/2.dim_shop/2.dim_shop.sql
+++ scripts_sql/dimensions/2.dim_shop/2.dim_shop.sql
@@ -1,20 +1,27 @@
---Create table
-DROP TABLE [computer_stg].[dbo].[dim_shop] CREATE TABLE [computer_stg].[dbo].[dim_shop](
-	[ShopID] INT IDENTITY (1, 1) PRIMARY KEY,
-	[Shop_Name] [nvarchar](50) NOT NULL,
-	[Shop_Age] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from the raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_shop] ([Shop_Name], [Shop_Age])
-SELECT
-	DISTINCT [Shop_Name],
-	[Shop_Age]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the shops
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_shop]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_shop]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_shop](
+        [ShopID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Shop_Name] NVARCHAR(50) NOT NULL,
+        [Shop_Age] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_shop] (
+    [Shop_Name],
+    [Shop_Age]
+)
+SELECT DISTINCT
+    [Shop_Name],
+    [Shop_Age]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the shops
+SELECT *
+FROM [computer_stg].[dbo].[dim_shop];
+GO

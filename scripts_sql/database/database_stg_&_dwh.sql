f2a38c7d46d6db7ecbb88741d2d3d83fc27808bc
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:19:12 +0200

    cleaning database script

--- scripts_sql/database/database_stg_&_dwh.sql
+++ scripts_sql/database/database_stg_&_dwh.sql
@@ -1,12 +1,13 @@
---CREATE DATABASE computer_stg
---CREATE DATABASE computer_dwh
---
-IF DB_ID('computer_stg') IS NULL BEGIN CREATE DATABASE computer_stg;
-
+-- Create computer_stg if it does not exist
+IF DB_ID('computer_stg') IS NULL
+BEGIN
+    CREATE DATABASE computer_stg;
 END
 GO
---
-    IF DB_ID('computer_dwh') IS NULL BEGIN CREATE DATABASE computer_dwh;
 
+-- Create computer_dwh if it does not exist
+IF DB_ID('computer_dwh') IS NULL
+BEGIN
+    CREATE DATABASE computer_dwh;
 END
 GO

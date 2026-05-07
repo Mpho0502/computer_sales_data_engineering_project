cfe34d28ee340ea052948bd9a95879131ec77657
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:26:43 +0200

    cleaning dim_location script

--- scripts_sql/dimensions/1.dim_location/1.dim_location.sql
+++ scripts_sql/dimensions/1.dim_location/1.dim_location.sql
@@ -1,26 +1,30 @@
---create table
-DROP TABLE [computer_stg].[dbo].[dim_location] CREATE TABLE [computer_stg].[dbo].[dim_location](
-	[LocationID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Continent] [nvarchar](50) NOT NULL,
-	[Country_or_State] [nvarchar](50) NOT NULL,
-	[Province_or_City] [nvarchar](100) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from Raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_location] (
-		[Continent],
-		[Country_or_State],
-		[Province_or_City]
-	)
-SELECT
-	DISTINCT [Continent],
-	[Country_or_State],
-	[Province_or_City]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the locations
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_location]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_location]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_location](
+        [LocationID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Continent] NVARCHAR(50) NOT NULL,
+        [Country_or_State] NVARCHAR(50) NOT NULL,
+        [Province_or_City] NVARCHAR(100) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from Raw data
+INSERT INTO [computer_stg].[dbo].[dim_location] (
+    [Continent],
+    [Country_or_State],
+    [Province_or_City]
+)
+SELECT DISTINCT
+    [Continent],
+    [Country_or_State],
+    [Province_or_City]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the locations
+SELECT *
+FROM [computer_stg].[dbo].[dim_location];
+GO

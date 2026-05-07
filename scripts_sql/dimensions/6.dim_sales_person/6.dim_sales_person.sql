8f5b698c86b537ca073ae4618b96868ffe0e69cf
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:43:08 +0200

    cleaning dim_sales script

--- scripts_sql/dimensions/6.dim_sales_person/6.dim_sales_person.sql
+++ scripts_sql/dimensions/6.dim_sales_person/6.dim_sales_person.sql
@@ -1,23 +1,27 @@
---create table
-DROP TABLE [computer_stg].[dbo].[dim_sales_person] CREATE TABLE [computer_stg].[dbo].[dim_sales_person](
-	[SalesPersonID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Sales_Person_Name] [nvarchar](50) NOT NULL,
-	[Sales_Person_Department] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into tables from raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_sales_person](
-		[Sales_Person_Name],
-		[Sales_Person_Department]
-	)
-SELECT
-	DISTINCT [Sales_Person_Name],
-	[Sales_Person_Department]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the sales persons
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_sales_person]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_sales_person]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_sales_person](
+        [SalesPersonID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Sales_Person_Name] NVARCHAR(50) NOT NULL,
+        [Sales_Person_Department] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_sales_person] (
+    [Sales_Person_Name],
+    [Sales_Person_Department]
+)
+SELECT DISTINCT
+    [Sales_Person_Name],
+    [Sales_Person_Department]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the sales persons
+SELECT *
+FROM [computer_stg].[dbo].[dim_sales_person];
+GO

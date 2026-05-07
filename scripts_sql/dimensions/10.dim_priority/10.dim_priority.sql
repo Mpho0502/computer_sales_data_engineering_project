b143b5923a08fd26afca70b0632c406be310cf07
Mpho <matebesimoe89@gmail.com>
2026-05-07 19:02:59 +0200

    cleaning dim_priority script

--- scripts_sql/dimensions/10.dim_priority/10.dim_priority.sql
+++ scripts_sql/dimensions/10.dim_priority/10.dim_priority.sql
@@ -1,18 +1,24 @@
---create table
-DROP TABLE [computer_stg].[dbo].[dim_priority] CREATE TABLE [computer_stg].[dbo].[dim_priority](
-	[PriorityID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Priority] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from the raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_priority]([Priority])
-SELECT
-	DISTINCT [Priority]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the priorities
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_priority]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_priority]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_priority](
+        [PriorityID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Priority] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_priority] (
+    [Priority]
+)
+SELECT DISTINCT
+    [Priority]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the priorities
+SELECT *
+FROM [computer_stg].[dbo].[dim_priority];
+GO

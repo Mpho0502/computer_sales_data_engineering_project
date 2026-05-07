dd46d2ccf865387faea1cf5ade04e4cdcb246afd
Mpho <matebesimoe89@gmail.com>
2026-05-07 18:38:33 +0200

    cleaning dim_storage script

--- scripts_sql/dimensions/4.dim_storage/4.dim_storage.sql
+++ scripts_sql/dimensions/4.dim_storage/4.dim_storage.sql
@@ -1,26 +1,30 @@
---create table
-DROP TABLE [computer_stg].[dbo].[dim_storage] CREATE TABLE [computer_stg].[dbo].[dim_storage](
-	[StorageID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Storage_Type] [nvarchar](50) NOT NULL,
-	[RAM] [nvarchar](50) NOT NULL,
-	[Storage_Capacity] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
---Insert values into the table from raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_storage](
-		[Storage_Type],
-		[RAM],
-		[Storage_Capacity]
-	)
-SELECT
-	DISTINCT [Storage_Type],
-	[RAM],
-	[Storage_Capacity]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
---Show all the storage insight
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_storage]
+-- Create table if it does not exist
+IF OBJECT_ID('[computer_stg].[dbo].[dim_storage]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [computer_stg].[dbo].[dim_storage](
+        [StorageID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [Storage_Type] NVARCHAR(50) NOT NULL,
+        [RAM] NVARCHAR(50) NOT NULL,
+        [Storage_Capacity] NVARCHAR(50) NOT NULL,
+        [Load_date] DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+-- Insert distinct values into the table from raw data
+INSERT INTO [computer_stg].[dbo].[dim_storage] (
+    [Storage_Type],
+    [RAM],
+    [Storage_Capacity]
+)
+SELECT DISTINCT
+    [Storage_Type],
+    [RAM],
+    [Storage_Capacity]
+FROM [computer_stg].[dbo].[raw_pc_data];
+GO
+
+-- Show all the storage insight
+SELECT *
+FROM [computer_stg].[dbo].[dim_storage];
+GO

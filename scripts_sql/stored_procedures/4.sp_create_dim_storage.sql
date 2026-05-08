31da8ecec0f5626ad6c383bce49b74ddf3ba7e14
Mpho <matebesimoe89@gmail.com>
2026-05-08 12:51:45 +0200

    cleaning sp create dim_storage script

--- scripts_sql/stored_procedures/4.sp_create_dim_storage.sql
+++ scripts_sql/stored_procedures/4.sp_create_dim_storage.sql
@@ -1,31 +1,35 @@
+-- Create stored procedure to build and populate dim_storage
 CREATE PROCEDURE sp_create_dim_storage
 AS
 BEGIN
-  -- SQL_statements to be executed
-		--create table
-DROP TABLE [computer_stg].[dbo].[dim_storage] CREATE TABLE [computer_stg].[dbo].[dim_storage](
-	[StorageID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Storage_Type] [nvarchar](50) NOT NULL,
-	[RAM] [nvarchar](50) NOT NULL,
-	[Storage_Capacity] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
-		--Insert values into the table from raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_storage](
-		[Storage_Type],
-		[RAM],
-		[Storage_Capacity]
-	)
-SELECT
-	DISTINCT [Storage_Type],
-	[RAM],
-	[Storage_Capacity]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
-		--Show all the storage insight
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_storage]
-END;
+    SET NOCOUNT ON;
+
+    -- Create table if it does not exist
+    IF OBJECT_ID('[computer_stg].[dbo].[dim_storage]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [computer_stg].[dbo].[dim_storage](
+            [StorageID] INT IDENTITY(1, 1) PRIMARY KEY,
+            [Storage_Type] NVARCHAR(50) NOT NULL,
+            [RAM] NVARCHAR(50) NOT NULL,
+            [Storage_Capacity] NVARCHAR(50) NOT NULL,
+            [Load_date] DATETIME DEFAULT GETDATE()
+        );
+    END
+
+    -- Insert distinct values into the table from raw data
+    INSERT INTO [computer_stg].[dbo].[dim_storage] (
+        [Storage_Type],
+        [RAM],
+        [Storage_Capacity]
+    )
+    SELECT DISTINCT
+        [Storage_Type],
+        [RAM],
+        [Storage_Capacity]
+    FROM [computer_stg].[dbo].[raw_pc_data];
+
+    -- Show all the storage insight
+    SELECT *
+    FROM [computer_stg].[dbo].[dim_storage];
+END
+GO

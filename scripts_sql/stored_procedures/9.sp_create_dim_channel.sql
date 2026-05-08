e6acb3818aa664e591da38322d625a8152f90d2c
Mpho <matebesimoe89@gmail.com>
2026-05-08 13:16:38 +0200

    cleaning sp create dim_channel script

--- scripts_sql/stored_procedures/9.sp_create_dim_channel.sql
+++ scripts_sql/stored_procedures/9.sp_create_dim_channel.sql
@@ -1,23 +1,29 @@
+-- Create stored procedure to build and populate dim_channel
 CREATE PROCEDURE sp_create_dim_channel
 AS
 BEGIN
-  -- SQL_statements to be executed
-	--create table
-DROP TABLE [computer_stg].[dbo].[dim_channel] CREATE TABLE [computer_stg].[dbo].[dim_channel](
-	[Channel_ID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Channel] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
-	--Insert values into the table from the raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_channel]([Channel])
-SELECT
-	DISTINCT [Channel]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
-	--Show all the channels
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_channel]
-END;
+    SET NOCOUNT ON;
+
+    -- Create table if it does not exist
+    IF OBJECT_ID('[computer_stg].[dbo].[dim_channel]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [computer_stg].[dbo].[dim_channel](
+            [Channel_ID] INT IDENTITY(1, 1) PRIMARY KEY,
+            [Channel] NVARCHAR(50) NOT NULL,
+            [Load_date] DATETIME DEFAULT GETDATE()
+        );
+    END
+
+    -- Insert distinct values into the table from raw data
+    INSERT INTO [computer_stg].[dbo].[dim_channel] (
+        [Channel]
+    )
+    SELECT DISTINCT
+        [Channel]
+    FROM [computer_stg].[dbo].[raw_pc_data];
+
+    -- Show all the channels
+    SELECT *
+    FROM [computer_stg].[dbo].[dim_channel];
+END
+GO

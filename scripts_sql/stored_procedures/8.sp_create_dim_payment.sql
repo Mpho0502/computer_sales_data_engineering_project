97c75ea492950a201f9524f1d7dc5a20b2b5de01
Mpho <matebesimoe89@gmail.com>
2026-05-08 13:11:24 +0200

    cleaning sp create dim_payment script

--- scripts_sql/stored_procedures/8.sp_create_dim_payment.sql
+++ scripts_sql/stored_procedures/8.sp_create_dim_payment.sql
@@ -1,23 +1,29 @@
+-- Create stored procedure to build and populate dim_payment
 CREATE PROCEDURE sp_create_dim_payment
 AS
 BEGIN
-  -- SQL_statements to be executed
-	--create table
-DROP TABLE [computer_stg].[dbo].[dim_payment] CREATE TABLE [computer_stg].[dbo].[dim_payment](
-	[PaymentID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Payment_Method] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
-	--Insert values into the table from the raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_payment]([Payment_Method])
-SELECT
-	DISTINCT [Payment_Method]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
-	--Show all payment methods
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_payment]
-END;
+    SET NOCOUNT ON;
+
+    -- Create table if it does not exist
+    IF OBJECT_ID('[computer_stg].[dbo].[dim_payment]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [computer_stg].[dbo].[dim_payment](
+            [PaymentID] INT IDENTITY(1, 1) PRIMARY KEY,
+            [Payment_Method] NVARCHAR(50) NOT NULL,
+            [Load_date] DATETIME DEFAULT GETDATE()
+        );
+    END
+
+    -- Insert distinct values into the table from raw data
+    INSERT INTO [computer_stg].[dbo].[dim_payment] (
+        [Payment_Method]
+    )
+    SELECT DISTINCT
+        [Payment_Method]
+    FROM [computer_stg].[dbo].[raw_pc_data];
+
+    -- Show all payment methods
+    SELECT *
+    FROM [computer_stg].[dbo].[dim_payment];
+END
+GO

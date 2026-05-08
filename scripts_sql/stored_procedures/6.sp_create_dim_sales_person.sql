1602a62d3a40fb3b5fb7e9766b0d707bd552bcc2
Mpho <matebesimoe89@gmail.com>
2026-05-08 12:59:34 +0200

    cleaning sp create dim_sales_person script

--- scripts_sql/stored_procedures/6.sp_create_dim_sales_person.sql
+++ scripts_sql/stored_procedures/6.sp_create_dim_sales_person.sql
@@ -1,28 +1,32 @@
+-- Create stored procedure to build and populate dim_sales_person
 CREATE PROCEDURE sp_create_dim_sales_person
 AS
 BEGIN
-  -- SQL_statements to be executed
-	--create table
-DROP TABLE [computer_stg].[dbo].[dim_sales_person] CREATE TABLE [computer_stg].[dbo].[dim_sales_person](
-	[SalesPersonID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[Sales_Person_Name] [nvarchar](50) NOT NULL,
-	[Sales_Person_Department] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
-	--Insert values into tables from raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_sales_person](
-		[Sales_Person_Name],
-		[Sales_Person_Department]
-	)
-SELECT
-	DISTINCT [Sales_Person_Name],
-	[Sales_Person_Department]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
-	--Show all the sales persons
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_sales_person]
-END;
+    SET NOCOUNT ON;
+
+    -- Create table if it does not exist
+    IF OBJECT_ID('[computer_stg].[dbo].[dim_sales_person]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [computer_stg].[dbo].[dim_sales_person](
+            [SalesPersonID] INT IDENTITY(1, 1) PRIMARY KEY,
+            [Sales_Person_Name] NVARCHAR(50) NOT NULL,
+            [Sales_Person_Department] NVARCHAR(50) NOT NULL,
+            [Load_date] DATETIME DEFAULT GETDATE()
+        );
+    END
+
+    -- Insert distinct values into the table from raw data
+    INSERT INTO [computer_stg].[dbo].[dim_sales_person] (
+        [Sales_Person_Name],
+        [Sales_Person_Department]
+    )
+    SELECT DISTINCT
+        [Sales_Person_Name],
+        [Sales_Person_Department]
+    FROM [computer_stg].[dbo].[raw_pc_data];
+
+    -- Show all the sales persons
+    SELECT *
+    FROM [computer_stg].[dbo].[dim_sales_person];
+END
+GO

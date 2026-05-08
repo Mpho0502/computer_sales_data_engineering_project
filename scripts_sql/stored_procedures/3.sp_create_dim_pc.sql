8f7b6509651d8f4739a7bea219e83d611299a60a
Mpho <matebesimoe89@gmail.com>
2026-05-08 12:48:45 +0200

    cleaning sp create dim_pc script

--- scripts_sql/stored_procedures/3.sp_create_dim_pc.sql
+++ scripts_sql/stored_procedures/3.sp_create_dim_pc.sql
@@ -1,25 +1,32 @@
+-- Create stored procedure to build and populate dim_pc
 CREATE PROCEDURE sp_create_dim_pc
 AS
 BEGIN
-  -- SQL_statements to be executed
-	 --Create table
-DROP TABLE [computer_stg].[dbo].[dim_pc] CREATE TABLE [computer_stg].[dbo].[dim_pc](
-	[PC_ID] INT IDENTITY(1, 1) PRIMARY KEY,
-	[PC_Make] [nvarchar](50) NOT NULL,
-	[PC_Model] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
-		--Insert values into the table from raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_pc]([PC_Make], [PC_Model])
-SELECT
-	DISTINCT [PC_Make],
-	[PC_Model]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
-		--Show all the PCs
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_pc]
-END;
+    SET NOCOUNT ON;
+
+    -- Create table if it does not exist
+    IF OBJECT_ID('[computer_stg].[dbo].[dim_pc]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [computer_stg].[dbo].[dim_pc](
+            [PC_ID] INT IDENTITY(1, 1) PRIMARY KEY,
+            [PC_Make] NVARCHAR(50) NOT NULL,
+            [PC_Model] NVARCHAR(50) NOT NULL,
+            [Load_date] DATETIME DEFAULT GETDATE()
+        );
+    END
+
+    -- Insert distinct values into the table from raw data
+    INSERT INTO [computer_stg].[dbo].[dim_pc] (
+        [PC_Make],
+        [PC_Model]
+    )
+    SELECT DISTINCT
+        [PC_Make],
+        [PC_Model]
+    FROM [computer_stg].[dbo].[raw_pc_data];
+
+    -- Show all the PCs
+    SELECT *
+    FROM [computer_stg].[dbo].[dim_pc];
+END
+GO

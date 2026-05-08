49aff3fa214abf5806c625f617bc4fc12f0d5121
Mpho <matebesimoe89@gmail.com>
2026-05-08 13:57:26 +0200

    creating and cleaning sp to execute all dimensions and fact table script

---
+++ scripts_sql/stored_procedures/sp_executions/sp_executions_for_all_dimensions_and_fact_table.sql
@@ -0,0 +1,22 @@
+-- Create master stored procedure to refresh all dimensions and fact table
+CREATE PROCEDURE sp_executions_for_all_dimensions_and_fact_table
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Refresh dimension tables
+    EXEC sp_create_dim_location;
+    EXEC sp_create_dim_shop;
+    EXEC sp_create_dim_pc;
+    EXEC sp_create_dim_storage;
+    EXEC sp_create_dim_customer;
+    EXEC sp_create_dim_sales_person;
+    EXEC sp_create_dim_date;
+    EXEC sp_create_dim_payment;
+    EXEC sp_create_dim_channel;
+    EXEC sp_create_dim_priority;
+
+    -- Refresh fact table
+    EXEC sp_create_pc_sales_fact
+END
+GO

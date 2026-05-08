a2e5798d559f56c80e1bf3dd7533d480dacd0020
Mpho <matebesimoe89@gmail.com>
2026-05-08 12:42:48 +0200

    cleaning sp create dim_shop script

--- scripts_sql/stored_procedures/2.sp_create_dim_shop.sql
+++ scripts_sql/stored_procedures/2.sp_create_dim_shop.sql
@@ -1,26 +1,32 @@
+-- Create stored procedure to build and populate dim_shop
 CREATE PROCEDURE sp_create_dim_shop
- 
 AS
 BEGIN
-  -- SQL_statements to be executed
-		--Create table
-DROP TABLE [computer_stg].[dbo].[dim_shop] CREATE TABLE [computer_stg].[dbo].[dim_shop](
-	[ShopID] INT IDENTITY (1, 1) PRIMARY KEY,
-	[Shop_Name] [nvarchar](50) NOT NULL,
-	[Shop_Age] [nvarchar](50) NOT NULL,
-	[Load_date] DATETIME DEFAULT GETDATE()
-) 
-		--Insert values into the table from the raw data
-INSERT INTO
-	[computer_stg].[dbo].[dim_shop] ([Shop_Name], [Shop_Age])
-SELECT
-	DISTINCT [Shop_Name],
-	[Shop_Age]
-FROM
-	[computer_stg].[dbo].[raw_pc_data] 
-		--Show all the shops
-SELECT
-	*
-FROM
-	[computer_stg].[dbo].[dim_shop]
-END;
+    SET NOCOUNT ON;
+
+    -- Create table if it does not exist
+    IF OBJECT_ID('[computer_stg].[dbo].[dim_shop]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [computer_stg].[dbo].[dim_shop](
+            [ShopID] INT IDENTITY(1, 1) PRIMARY KEY,
+            [Shop_Name] NVARCHAR(50) NOT NULL,
+            [Shop_Age] NVARCHAR(50) NOT NULL,
+            [Load_date] DATETIME DEFAULT GETDATE()
+        );
+    END
+
+    -- Insert distinct values into the table from raw data
+    INSERT INTO [computer_stg].[dbo].[dim_shop] (
+        [Shop_Name],
+        [Shop_Age]
+    )
+    SELECT DISTINCT
+        [Shop_Name],
+        [Shop_Age]
+    FROM [computer_stg].[dbo].[raw_pc_data];
+
+    -- Show all the shops
+    SELECT *
+    FROM [computer_stg].[dbo].[dim_shop];
+END
+GO
